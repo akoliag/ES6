be8d87cc301d4b5eb16e4917b176558d9b6c4ead
Aga <a.kolia.grudzien@gmail.com>
2018-03-04 21:19:56 +0100

    material revision

---
+++ 9. string interpolation.js
@@ -0,0 +1,13 @@
+for(let number = 1; number < 5; number++) {
+
+    let button = document.createElement("button");
+    let size = (number * 100) + "%";
+    let button = `# ${number} - ${size}`;
+    button.innerText = buttonText;
+    document.body.appendChild(button);
+
+    button.addEventListener("click", function() {
+        console.log(`You picked ${buttonText}`);
+        document.body.style.fontSize = size;
+    })
+}
